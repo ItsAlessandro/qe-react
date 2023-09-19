3d11c980e827faa5e5128be7ff55b25c73a79a8e
TommasoTricker <trickertommaso@gmail.com>
2023-09-19 10:41:22 +0200

    refactored code, adjusted behaviour of PinCode

--- src/components/PinCode/index.tsx
+++ src/components/PinCode/index.tsx
@@ -3,55 +3,56 @@ import { useUrl } from "../../data/storage"
 import './PinCode.css'
 
 function PinCode() {
+    const placeholders = ['R', 'O', 'O', 'M', 'I', 'D'];
 
-    const  { roomUrl, updateUrl } = useUrl()
+    const { roomUrl, updateUrl } = useUrl()
 
-    function handleChange (event : React.ChangeEvent<HTMLInputElement>) {
-        let array : string[] = [...roomUrl]
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        let array: string[] = [...roomUrl]
         if (event.target.value.length >= 1) {
+            array[parseInt(event.target.id)] = array[parseInt(event.target.id)]
+                ? event.target.value[1].toUpperCase()
+                : event.target.value[0].toUpperCase()
 
-            array[parseInt(event.target.id)] = array[parseInt(event.target.id)] 
-            ? event.target.value[1].toUpperCase() 
-            : event.target.value[0].toUpperCase()
-
-            if (parseInt(event.target.id) !== 5) 
+            if (parseInt(event.target.id) !== 5)
                 document.getElementById((parseInt(event.target.id) + 1).toString())?.focus()
-            else 
-                document.getElementById(event.target.id)?.blur()
+
         }
         updateUrl(array)
     }
 
-    function handleBackspace (event : React.KeyboardEvent<HTMLInputElement>) {
+    function handleBackspace(event: React.KeyboardEvent<HTMLInputElement>) {
         if (event.keyCode === 8) {
             let array = [...roomUrl]
             array[parseInt(event.currentTarget.id)] = ''
             updateUrl(array)
 
-            if (event.currentTarget.id !== '0') 
+            if (event.currentTarget.id !== '0')
                 document.getElementById((parseInt(event.currentTarget.id) - 1).toString())?.focus()
         }
     }
 
+    function handleClick(event: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+        var inputElement = event.target as HTMLInputElement;
+        var temp_value = inputElement.value;
+        inputElement.value = '';
+        inputElement.value = temp_value;
+    }
+
     return (
         <div className='pincode'>
-            <input type='text' placeholder='R' value={roomUrl[0]} id='0' className='pincode-input'
-            onChange={(e) => handleChange(e)}  onKeyDown={(e) => handleBackspace(e)} />
-
-            <input type='text' placeholder='O' value={roomUrl[1]} id='1' className='pincode-input'
-            onChange={(e) => handleChange(e)}  onKeyDown={(e) => handleBackspace(e)} />
-
-            <input type='text' placeholder='O' value={roomUrl[2]} id='2' className='pincode-input'
-            onChange={(e) => handleChange(e)}  onKeyDown={(e) => handleBackspace(e)} />
-
-            <input type='text' placeholder='M' value={roomUrl[3]} id='3' className='pincode-input'
-            onChange={(e) => handleChange(e)}  onKeyDown={(e) => handleBackspace(e)} />
-
-            <input type='text' placeholder='I' value={roomUrl[4]} id='4' className='pincode-input'
-            onChange={(e) => handleChange(e)}  onKeyDown={(e) => handleBackspace(e)} />
-
-            <input type='text' placeholder='D' value={roomUrl[5]} id='5' className='pincode-input'
-            onChange={(e) => handleChange(e)}  onKeyDown={(e) => handleBackspace(e)} />
+            {placeholders.map((placeholder, index) => (
+                <input
+                    type='text'
+                    placeholder={placeholder}
+                    value={roomUrl[index]}
+                    id={index.toString()}
+                    className='pincode-input'
+                    onChange={(e) => handleChange(e)}
+                    onKeyDown={(e) => handleBackspace(e)}
+                    onClick={(e) => handleClick(e)}
+                />
+            ))}
         </div>
     )
 }
