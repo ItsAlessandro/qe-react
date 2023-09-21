7fdecf9e677dad46002c1e239b78aa9e07a21c40
Sir. Duddo <aleduranti005@gmail.com>
2023-09-21 10:52:22 +0200

    Lobby completed, 'Lockdown' & 'Harden' browser errors

--- src/components/PinCode/index.tsx
+++ src/components/PinCode/index.tsx
@@ -2,48 +2,46 @@ import { useUrl } from "../../data/storage"
 
 import './PinCode.css'
 
-function PinCode({ enabled }: { enabled: boolean }) {
-    const { roomUrl, updateUrl } = useUrl()
+function PinCode({enabled} : {enabled : boolean}) {
+
+    const  { roomUrl, updateUrl } = useUrl()
     const placeholders = ['R', 'O', 'O', 'M', 'I', 'D'];
 
-    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handleChange (event : React.ChangeEvent<HTMLInputElement>) {
         if (enabled) {
-            let array: string[] = [...roomUrl]
+            let array : string[] = [...roomUrl]
             if (event.target.value.length >= 1) {
-                array[parseInt(event.target.id)] = array[parseInt(event.target.id)]
-                    ? event.target.value[1].toUpperCase()
-                    : event.target.value[0].toUpperCase()
 
-                if (parseInt(event.target.id) !== 5)
+                array[parseInt(event.target.id)] = array[parseInt(event.target.id)] 
+                ? event.target.value[1].toUpperCase() 
+                : event.target.value[0].toUpperCase()
+
+                if (parseInt(event.target.id) !== 5) 
                     document.getElementById((parseInt(event.target.id) + 1).toString())?.focus()
+                else 
+                    document.getElementById(event.target.id)?.blur()
             }
             updateUrl(array)
         }
     }
 
-    function handleBackspace(event: React.KeyboardEvent<HTMLInputElement>) {
+    function handleBackspace (event : React.KeyboardEvent<HTMLInputElement>) {
         if (enabled) {
             if (event.keyCode === 8) {
                 let array = [...roomUrl]
                 array[parseInt(event.currentTarget.id)] = ''
                 updateUrl(array)
-                if (event.currentTarget.id !== '0')
+                if (event.currentTarget.id !== '0') 
                     document.getElementById((parseInt(event.currentTarget.id) - 1).toString())?.focus()
             }
         }
-    }
-
-    function handleClick(event: React.MouseEvent<HTMLInputElement, MouseEvent>) {
-        var inputElement = event.target as HTMLInputElement;
-        var temp_value = inputElement.value;
-        inputElement.value = '';
-        inputElement.value = temp_value;
     }
 
     return (
         <div className='pincode'>
             {placeholders.map((placeholder, index) => (
                 <input
+                    key={index}
                     type='text'
                     placeholder={placeholder}
                     value={roomUrl[index]}
@@ -51,7 +49,7 @@ function PinCode({ enabled }: { enabled: boolean }) {
                     className='pincode-input'
                     onChange={(e) => handleChange(e)}
                     onKeyDown={(e) => handleBackspace(e)}
-                    onClick={(e) => handleClick(e)}
+                    style={!enabled ? {outline: 'none'} : {}}
                 />
             ))}
         </div>
--- src/pages/Lobby/index.tsx
+++ src/pages/Lobby/index.tsx
@@ -5,11 +5,13 @@ import PinCode from '../../components/PinCode'
 import Request from '../../components/Request'
 import home from '../../images/home.svg'
 import { db } from '../../data/firebase'
-import { doc, updateDoc, onSnapshot, getDoc, collection, query, where } from 'firebase/firestore'
+import { doc, updateDoc, onSnapshot, getDoc, deleteDoc } from 'firebase/firestore'
 import { useLobbyFinder } from '../../data/storage'
 
 import '../Home/Home.css'
 import './Lobby.css'
+
+let removedLobby = false
 
 function Lobby() {
 
@@ -20,7 +22,9 @@ function Lobby() {
     const navigate = useNavigate()
 
     async function handleRemoveLobby () {
-        
+        removedLobby = true
+        await deleteDoc(doc(db, 'sessions', currentLobby))
+        navigate('/')
     }
 
     async function handleExit (name : string) {
@@ -58,40 +62,43 @@ function Lobby() {
 
     useEffect(() => {
         const unsub = onSnapshot(doc(db, 'sessions', currentLobby), (doc) => {
-            let playersToDisplay = [...doc.data()?.gamePlayers, ...doc.data()?.gamePending]
-            let counter = 0;
-            playersToDisplay = playersToDisplay.map(
-                function (val: string, index : number) {
-                    if (index < doc.data()?.gamePlayers.length) {
-                        return (
-                            <Request
-                                key={index}
-                                index={index}
-                                colour={'#FFFFFF'}
-                                nickname={val}
-                                type={0} // 0 = joined player
-                                rejectPlayer={rejectPlayer}
-                                acceptPlayer={acceptPlayer}
-                            />
-                        )
-                    } else {
-                        return (
-                            <Request
-                                key={index}
-                                index={counter++}
-                                colour={'#FFFFFF'}
-                                nickname={val}
-                                type={1} // 1 = pending player
-                                rejectPlayer={rejectPlayer}
-                                acceptPlayer={acceptPlayer}
-                            />
-                        )
+            console.log(removedLobby)
+            if (!removedLobby) {
+                let playersToDisplay = [...doc.data()?.gamePlayers, ...doc.data()?.gamePending]
+                let counter = 0;
+                playersToDisplay = playersToDisplay.map(
+                    function (val: string, index : number) {
+                        if (index < doc.data()?.gamePlayers.length) {
+                            return (
+                                <Request
+                                    key={index}
+                                    index={index}
+                                    colour={'#FFFFFF'}
+                                    nickname={val}
+                                    type={0} // 0 = joined player
+                                    rejectPlayer={rejectPlayer}
+                                    acceptPlayer={acceptPlayer}
+                                />
+                            )
+                        } else if (isOwner) {
+                            return (
+                                <Request
+                                    key={index}
+                                    index={counter++}
+                                    colour={'#FFFFFF'}
+                                    nickname={val}
+                                    type={1} // 1 = pending player
+                                    rejectPlayer={rejectPlayer}
+                                    acceptPlayer={acceptPlayer}
+                                />
+                            )
 
+                        }
+                        
                     }
-                    
-                }
-            )
-            setPending(playersToDisplay)
+                )
+                setPending(playersToDisplay)
+            }
         })
         return () => { unsub() }
     }, [])
