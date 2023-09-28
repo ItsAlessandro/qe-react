bf51add4556ecf00f68ce9abce6cbdf5a62ff385
TommasoTricker <trickertommaso@gmail.com>
2023-09-28 13:02:59 +0200

    Fixed PinCode actually for real this time

--- src/components/PinCode/index.tsx
+++ src/components/PinCode/index.tsx
@@ -2,46 +2,45 @@ import { useUrl } from "../../data/storage"
 
 import './PinCode.css'
 
-function PinCode({enabled} : {enabled : boolean}) {
-
-    const  { roomUrl, updateUrl } = useUrl()
+function PinCode({ enabled }: { enabled: boolean }) {
+    const { roomUrl, updateUrl } = useUrl()
     const placeholders = ['R', 'O', 'O', 'M', 'I', 'D'];
 
-    function handleChange (event : React.ChangeEvent<HTMLInputElement>) {
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
         if (enabled) {
-            let array : string[] = [...roomUrl]
-            if (event.target.value.length >= 1) {
+            let array: string[] = [...roomUrl]
 
-                array[parseInt(event.target.id)] = array[parseInt(event.target.id)] 
-                ? event.target.value[1].toUpperCase() 
-                : event.target.value[0].toUpperCase()
-
-                if (parseInt(event.target.id) !== 5) 
-                    document.getElementById((parseInt(event.target.id) + 1).toString())?.focus()
-                else 
-                    document.getElementById(event.target.id)?.blur()
-            }
+            array[parseInt(event.target.id)] = (event.target.value.length > 1) ? event.target.value[1].toUpperCase() : event.target.value.toUpperCase()
             updateUrl(array)
+            
+            if (parseInt(event.target.id) !== 5)
+                document.getElementById((parseInt(event.target.id) + 1).toString())?.focus()
         }
     }
 
-    function handleBackspace (event : React.KeyboardEvent<HTMLInputElement>) {
+    function handleBackspace(event: React.KeyboardEvent<HTMLInputElement>) {
         if (enabled) {
             if (event.keyCode === 8) {
                 let array = [...roomUrl]
                 array[parseInt(event.currentTarget.id)] = ''
                 updateUrl(array)
-                if (event.currentTarget.id !== '0') 
+                if (event.currentTarget.id !== '0')
                     document.getElementById((parseInt(event.currentTarget.id) - 1).toString())?.focus()
             }
         }
+    }
+
+    function handleClick(event: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+        var inputElement = event.target as HTMLInputElement;
+        var temp_value = inputElement.value;
+        inputElement.value = '';
+        inputElement.value = temp_value;
     }
 
     return (
         <div className='pincode'>
             {placeholders.map((placeholder, index) => (
                 <input
-                    key={index}
                     type='text'
                     placeholder={placeholder}
                     value={roomUrl[index]}
@@ -49,7 +48,7 @@ function PinCode({enabled} : {enabled : boolean}) {
                     className='pincode-input'
                     onChange={(e) => handleChange(e)}
                     onKeyDown={(e) => handleBackspace(e)}
-                    style={!enabled ? {outline: 'none'} : {}}
+                    onClick={(e) => handleClick(e)}
                 />
             ))}
         </div>
